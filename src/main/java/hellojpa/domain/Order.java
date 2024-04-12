3ce6f5eb784e7a1a231d43434f39e0384db5e54f
dobidugi <dobidugi@gmail.com>
2024-04-12 12:01:07 +0900

    연관 관계 설정

--- src/main/java/hellojpa/domain/Order.java
+++ src/main/java/hellojpa/domain/Order.java
@@ -3,14 +3,22 @@ package hellojpa.domain;
 import jakarta.persistence.*;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Order {
 
   @Id @GeneratedValue
   @Column(name = "order_id")
   private Long id;
-  @Column(name = "member_id")
-  private Long memberId;
+
+  @ManyToOne
+  @JoinColumn(name = "member_id")
+  private Member member;
+
+  @OneToMany(mappedBy = "order")
+  private List<OrderItem> orderItems = new ArrayList<>();
+
   private LocalDateTime orderDate;
   @Enumerated(EnumType.STRING)
   private OrderState status;
@@ -21,14 +29,6 @@ public class Order {
 
   public void setId(Long id) {
     this.id = id;
-  }
-
-  public Long getMemberId() {
-    return memberId;
-  }
-
-  public void setMemberId(Long memberId) {
-    this.memberId = memberId;
   }
 
   public LocalDateTime getOrderDate() {
@@ -45,5 +45,10 @@ public class Order {
 
   public void setStatus(OrderState status) {
     this.status = status;
+  }
+
+  public void addOrderItem(OrderItem orderItem) {
+    orderItems.add(orderItem);
+    orderItem.setOrder(this);
   }
 }
--- src/main/java/hellojpa/domain/OrderItem.java
+++ src/main/java/hellojpa/domain/OrderItem.java
@@ -1,8 +1,6 @@
 package hellojpa.domain;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 public class OrderItem {
 
@@ -10,9 +8,14 @@ public class OrderItem {
   @Column(name = "order_item_id")
   private Long id;
 
-  @Column(name = "order_id")
-  private Long orderId;
-  private Long itemId;
+  @ManyToOne
+  @JoinColumn(name = "order_id")
+  private Order order;
+
+  @ManyToOne
+  @JoinColumn(name = "item_id")
+  private Item item;
+
   private int orderPrice;
   private int count;
 
@@ -22,22 +25,6 @@ public class OrderItem {
 
   public void setId(Long id) {
     this.id = id;
-  }
-
-  public Long getOrderId() {
-    return orderId;
-  }
-
-  public void setOrderId(Long orderId) {
-    this.orderId = orderId;
-  }
-
-  public Long getItemId() {
-    return itemId;
-  }
-
-  public void setItemId(Long itemId) {
-    this.itemId = itemId;
   }
 
   public int getOrderPrice() {
@@ -54,5 +41,9 @@ public class OrderItem {
 
   public void setCount(int count) {
     this.count = count;
+  }
+
+  public void setOrder(Order order) {
+    this.order = order;
   }
 }
